19ee83639e43b3a9816fdf5d882c235b2cbdee80
Kaj van der Valk <kajvandervalk@gmail.com>
2025-07-31 09:20:55 +0200

    Frontend use .env
    Backend and frontend now use the same .env file for development.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,41 +1,35 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
 
 
+const getProxy = (backendUrl, websocketUrl) => ({
+  '^/api/.*/connect': {
+    target: websocketUrl,
+    rewriteWsOrigin: true,
+    changeOrigin: true,
+    ws: true,
+    rewrite: path => path.replace(/^\/api/, '')
+  },
+  '/api': {
+    target: backendUrl,
+    changeOrigin: true,
+    rewrite: (path) => path.replace(/^\/api/, '')
+  },
+})
+
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '^/api/.*/connect': {
-        target: "ws://backend:8000",
-        rewriteWsOrigin: true,
-        changeOrigin: true,
-        ws: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      },
-      '/api': {
-        target: "http://backend:8000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
-    }
-  },
-  preview: {
-    proxy: {
-      '^/api/.*/connect': {
-        target: "ws://backend:8000",
-        rewriteWsOrigin: true,
-        changeOrigin: true,
-        ws: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      },
-      '/api': {
-        target: "http://backend:8000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
-    }
-  },
-  plugins: [svgr(), react()],
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    envDir: '../',
+    server: {
+      // proxy: getProxy(env.BACKEND_URL, env.WEBSOCKET_URL)
+      proxy: getProxy('http://host.docker.internal:8000', env.WEBSOCKET_URL)
+    },
+    preview: {
+      proxy: getProxy(env.BACKEND_URL, env.WEBSOCKET_URL)
+    },
+    plugins: [svgr(), react()],
+  }
 })
