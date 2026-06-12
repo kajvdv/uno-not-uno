fe856c2fa3a5ca1f91c20e8be76656e62083ff48
Kaj van der Valk <kajvandervalk@gmail.com>
2026-06-12 14:04:38 +0200

    File based routing

--- frontend-vue/src/router/index.ts
+++ frontend-vue/src/router/index.ts
@@ -1,17 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { routes, handleHotUpdate } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      component: () => import('@/views/start/StartView.vue'),
-    },
-    {
-      path: '/new',
-      component: () => import('@/views/new/NewGameView.vue'),
-    },
-  ],
+  routes,
 })
+
+if (import.meta.hot) {
+  handleHotUpdate(router)
+}
 
 export default router
